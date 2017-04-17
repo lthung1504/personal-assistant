9556f8264d267c8484f311692cd902214e8ee8d4
Hyo-Hung Le <lthung1504@gmail.com>
2017-04-17 16:07:03 +0700

    test compositeDisposable

--- app/src/main/java/com/example/thanhhungle/personalassistant/MainActivity.java
+++ app/src/main/java/com/example/thanhhungle/personalassistant/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.thanhhungle.personalassistant;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String LINE_SEPARATOR = "\n";
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    TextView mTextView;
+
+    private final CompositeDisposable mCompositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +42,64 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                testDisposable();
             }
+        });
+
+        // bind item
+        mTextView = (TextView) findViewById(R.id.tvText);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+        mCompositeDisposable.clear();
+    }
+
+    void testDisposable() {
+        mCompositeDisposable.add(sampleObservable()
+                // run on background thread
+                .subscribeOn(Schedulers.io())
+                // be notified on the main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<String>() {
+                    @Override
+                    public void onComplete() {
+                        mTextView.append(" onComplete");
+                        mTextView.append(LINE_SEPARATOR);
+                        Log.d(TAG, " onComplete");
+                    }
+
+                    @Override
+                    public void onNext(String value) {
+                        mTextView.append(" onNext : value : " + value);
+                        mTextView.append(LINE_SEPARATOR);
+                        Log.d(TAG, " onNext value : " + value);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        mTextView.append(" onError : " + e.getMessage());
+                        mTextView.append(LINE_SEPARATOR);
+                        Log.d(TAG, " onError : " + e.getMessage());
+
+                    }
+                })
+        );
+    }
+
+    static Observable<String> sampleObservable() {
+        return Observable.defer(new Callable<ObservableSource<? extends String>>() {
+
+            @Override
+            public ObservableSource<? extends String> call() throws Exception {
+                // Do some long running operation
+                SystemClock.sleep(2000);
+                return Observable.just("one", "two", "three", "four", "five");
+            }
+
         });
     }
 
